302f596c35e0777cbf1e63a914c3fcef98e63c38
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-05-11 20:59:02 +0300

    refactor upload image middleware

--- middlewares/uploadImageMiddleware.js
+++ middlewares/uploadImageMiddleware.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const AppError = require('../utils/AppError');
 
-const uploadSingleImage = (field) => {
+const multerOptions = () => {
   const multerStorage = multer.memoryStorage();
   const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -12,21 +12,11 @@ const uploadSingleImage = (field) => {
   };
 
   const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
-  return upload.single(field);
+  return upload;
 };
 
-const uploadMixOfImages = (field) => {
-  const multerStorage = multer.memoryStorage();
-  const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-      return cb(null, true);
-    } else {
-      return cb(new AppError('Please upload a valid image', 400), false); // Updated error message
-    }
-  };
+const uploadSingleImage = (fieldName) => multerOptions().single(fieldName);
 
-  const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
-  return upload.fields(field);
-};
+const uploadMixOfImages = (arrayOfFields) => multerOptions().fields(arrayOfFields);
 
 module.exports = { uploadSingleImage, uploadMixOfImages };
